42dce77ae8a799810089d9d2c5c0f2db1758e8a6
kelpikz <ajithaprasad96@gmail.com>
2021-07-09 01:33:36 +0530

    Feat(bot): Add proper examples for the bot routes

--- backend/schema/messages.py
+++ backend/schema/messages.py
@@ -1,9 +1,8 @@
 import logging
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError, validator
 from typing import Any, List, Optional, Union
 from mongoengine import ObjectIdField
 from bson import ObjectId
-
 
 
 from schema.members import MemberInDBSchema, memberHelper
@@ -11,6 +10,7 @@ from schema.dbmodel import PyObjectId
 
 from models.members import Member
 from models.messages import Message
+
 
 class CreateMessageSchema(BaseModel):
     """Message Schema"""
@@ -20,88 +20,151 @@ class CreateMessageSchema(BaseModel):
     tags: Optional[List[str]] = []
     isReply: bool = Field(False)
     replies = []
-    author : str = Field(...)
+    author: str = Field(...)
     parentMessage: Optional[str]
 
-    # TODO: v v imp
-    # ? Add validators to thw schema
-    class Config:
-        schema_extra = {
-            # TODO: Kinda not clean, fix it
-            "examples" : {
-                "normalMessageSchema": {
+    @validator("author")
+    def checkIfAuthorContainsHash(cls, v):
+        if not "#" in v:
+            raise ValueError("Author name should contain a '#'")
+        return v
+
+    @validator("isReply")
+    def checkIfValidMessage(cls, v, values):
+        """Check if the message follows correct format"""
+
+        if v:
+            # message is a reply, check if it follows the required format
+            if "tags" in values and values["tags"] != []:
+                # reply shouldn't contain tags
+                raise ValueError(
+                    "A reply shouldn't contain tags. Check the data you have entered."
+                )
+
+            if not "parentMessage" in values:
+                # a reply should have a parent message
+                raise ValueError(
+                    "A reply should have a parent message. \
+                    The given reply doesn't contain a parent message."
+                )
+        else:
+            # the message is a discussion, check whether it has tags and return
+            if not "tags" in values:
+                raise ValueError(
+                    "A discussion should contain tags, \
+                    the given message doesn't contain any tags."
+                )
+        return v
+
+    @classmethod
+    def getExample(self):
+        examples = {
+            "normalMessageSchema": {
+                "summary": "Schema for a new Discussion",
+                "description": "**Schema Explanation** for a new Discussion",
+                "value": {
                     "messageId": "unique id of the message",
-                    "message" : "content of the message, length should be more then 20 characters",
+                    "message": "content of the message, length should be more then 20 characters",
                     "author": "discord handle of the user",
                     "tag": ["an", "array", "of", "all", "the", "tags"],
                 },
-                "normalMessage": {
+            },
+            "normalMessage": {
+                "summary": "New Discussion Example",
+                "description": "**Example** for a new Discussion body",
+                "value": {
                     "messageId": "12334",
-                    "message" : "This is the message content",
+                    "message": "This is the message content",
                     "author": "joe#1234",
                     "tags": ["an", "array", "of", "all", "the", "tags"],
                 },
-                "ReplySchema": {
+            },
+            "ReplySchema": {
+                "summary": "Schema for a new Reply",
+                "description": "**Schema Explanation** for a new Reply",
+                "value": {
                     "messageId": "unique id of the message",
-                    "message" : "content of the reply, more than 20 characters long",
+                    "message": "content of the reply, more than 20 characters long",
                     "author": "discord handle of the message author",
-                    "isReply": "True",
-                    "parentMessage" : "unique id of the message of the parent message "
+                    "isReply": True,
+                    "parentMessage": "unique id of the message of the parent message ",
                 },
-                "Reply": {
+            },
+            "Reply": {
+                "summary": "New Reply Example",
+                "description": "**Example** for a new Reply body",
+                "value": {
                     "messageId": "12335",
-                    "message" : "This is the content content of the reply",
+                    "message": "This is the content content of the reply",
                     "author": "joemama#1234",
-                    "isReply": "True",
-                    "parentMessage" : "12334"
-                }
-            }
+                    "isReply": True,
+                    "parentMessage": "12334",
+                },
+            },
         }
+        return examples
 
 
 class UpdateMessageSchema(BaseModel):
-    """ Update message schema """
-    
-    messageId : str = Field(...)
+    """Update message schema"""
+
+    messageId: str = Field(...)
     message: Optional[str] = None
     tags: Optional[List[str]] = None
 
-    class Config:
-        schema_extra =  {
-            "examples": {
-                "schema": {
+    @classmethod
+    def getExample(self):
+        examples = {
+            "schema": {
+                "summary": "Schema for a update message",
+                "description": "**Schema Explanation** for update message",
+                "value": {
                     "messageId": "unique id of the message you want to edit",
                     "message": "Content of the message",
                     "tag": ["an", "array", "of", "all", "the", "tags"],
                 },
-                "example_request" : {
+            },
+            "example_request": {
+                "summary": "Update Message Example",
+                "description": "**Example** for update message",
+                "value": {
                     "messageId": "12334",
-                    "message" : "This is the edited message content",
+                    "message": "This is the edited message content",
                     "tags": ["an", "array", "of", "all", "the", "tags"],
-                }
-            }
+                },
+            },
         }
+        return examples
+
 
 class DeleteMessageSchema(BaseModel):
-    """ Delete message schema """
-    
-    messageId : str = Field(...)
+    """Delete message schema"""
 
-    class Config:
-        schema_extra =  {
-            "examples" : {
-                "schema": {
+    messageId: str = Field(...)
+
+    @classmethod
+    def getExample(self):
+        examples = {
+            "schema": {
+                "summary": "Schema for a delete message",
+                "description": "**Schema Explanation** for delete message",
+                "value": {
                     "messageId": "unique id of the message you want to edit",
                 },
-                "example_request" : {
+            },
+            "example_request": {
+                "summary": "Delete Message Example",
+                "description": "**Example** for delete message",
+                "value": {
                     "messageId": "12334",
-                }
-            }
+                },
+            },
         }
+        return examples
 
 
 class MessageInDbSchema(BaseModel):
-    messageId : Union[PyObjectId, str] = Field(...)
+    messageId: Union[PyObjectId, str] = Field(...)
     mongoDocument: Optional[Any] = None
 
     message: str = Field(...)
@@ -120,28 +183,33 @@ class MessageInDbSchema(BaseModel):
             # author is already an instance of MemberInDBSchema, so just return
             return
         except Exception as e:
-            logging.error("Couldn't convert author to an instance of 'MemberInDBSchema' due to", e)
-            raise Exception("Couldn't convert author to an instance of 'MemberInDBSchema' due to", e)
+            logging.error(
+                "Couldn't convert author to an instance of 'MemberInDBSchema' due to", e
+            )
+            raise Exception(
+                "Couldn't convert author to an instance of 'MemberInDBSchema' due to", e
+            )
 
     class Config:
         arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str
-        }
+        json_encoders = {ObjectId: str}
+
 
 # https://pydantic-docs.helpmanual.io/usage/postponed_annotations/#:~:text=data%20structures%20with%20self-referencing%20models%20are%20also%20supported%2C%20provided%20the%20function%20update_forward_refs()
 MessageInDbSchema.update_forward_refs()
 
 # API Response Models
 class CreateMessageResponseModel(BaseModel):
-    success : bool = Field(...)
+    success: bool = Field(...)
 
 
 class UpdateMessageResponseModel(CreateMessageResponseModel):
     pass
 
+
 class DeleteMessageResponseModel(CreateMessageResponseModel):
     pass
+
 
 def messageHelper(message: Message):
     """Converts a single message document returned by a mongo to a dict"""
@@ -152,7 +220,7 @@ def messageHelper(message: Message):
         "author": MemberInDBSchema(**memberHelper(message.author)),
         "timestamp": message.timeStamp,
         "replies": messageListHelper(message.replies),
-        "mongoDocument": message
+        "mongoDocument": message,
     }
 
     messageDict["tags"] = message.tags
@@ -160,7 +228,6 @@ def messageHelper(message: Message):
     if message.parentMessage:
         # messageDict["parentMessage"] = MessageInDbSchema(**messageHelper(message.parentMessage))
         messageDict["isDiscussion"] = False
-    
 
     return messageDict
 
@@ -168,8 +235,7 @@ def messageHelper(message: Message):
 def messageListHelper(messages: List[Message]):
     """Converts a array of message document returned by a mongo to a list of MessageInDB pydantic obj"""
 
-    if(not messages or len(messages) == 0):
+    if not messages or len(messages) == 0:
         return []
-    
-    return [ MessageInDbSchema(**messageHelper(message)) for message in messages]
-    
+
+    return [MessageInDbSchema(**messageHelper(message)) for message in messages]
